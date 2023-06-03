8199c92dcfb078b2886564372aa0bcdd156f22d4
David Radjai <dradjai@yahoo.com>
2023-06-03 19:08:00 -0400

    made navbar mobile responsive

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,7 +1,7 @@
 import { UserStatus } from "../../App";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
-import { Navbar, Nav} from "react-bootstrap"
+import { Navbar, Nav, Container} from "react-bootstrap"
 
 
 export default function NavMenu() {
@@ -19,55 +19,56 @@ export default function NavMenu() {
 
   return(
     
-    <Navbar className="nav-bar">
-    
+    <Navbar className="nav-bar" expand="md">
+      <Container>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
       
-      <Nav>
-      {!user 
+      <Nav className="me-auto">
+        {!user 
 
-       ? <> <Nav.Item>
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-        </Nav.Item>
+        ? <> <Nav.Item>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+          </Nav.Item>
 
-        <Nav.Item>
-          <Nav.Link as={Link} to="/login">Login</Nav.Link>
-        </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav.Item>
 
-        <Nav.Item>
-          <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
-        </Nav.Item> 
-        </>
-        : <>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+          </Nav.Item> 
+          </>
+          : <>
+          
+          <Nav.Item className="nav-home">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item className="nav-brokers">
+            <Nav.Link as={Link} to="/brokers">Brokers</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item className="nav-requests">
+            <Nav.Link as={Link} to="/requests">Requests</Nav.Link>
+          </Nav.Item>
         
-        <Nav.Item className="nav-home">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-        </Nav.Item>
+          <Nav.Item className="nav-calc">
+            <Nav.Link as={Link} to="/calculator">Calculator</Nav.Link>
+          </Nav.Item>
 
-        <Nav.Item className="nav-brokers">
-          <Nav.Link as={Link} to="/brokers">Brokers</Nav.Link>
-        </Nav.Item>
-
-        <Nav.Item className="nav-requests">
-          <Nav.Link as={Link} to="/requests">Requests</Nav.Link>
-        </Nav.Item>
-      
-        <Nav.Item className="nav-calc">
-          <Nav.Link as={Link} to="/calculator">Calculator</Nav.Link>
-        </Nav.Item>
-
-        
-        <Nav.Item className="nav-user">
-        <Nav.Link>Welcome {user.fullName}</Nav.Link>
-        </Nav.Item>
-        
-        <Nav.Item className="nav-logout">
-          <Nav.Link onClick={logout}>Logout</Nav.Link>
-        </Nav.Item> 
-        </>}
+          
+          <Nav.Item className="nav-user">
+          <Nav.Link>Welcome {user.fullName}</Nav.Link>
+          </Nav.Item>
+          
+          <Nav.Item className="nav-logout">
+            <Nav.Link onClick={logout}>Logout</Nav.Link>
+          </Nav.Item> 
+          </>}
         </Nav>
-
-  
-
+        </Navbar.Collapse>
+        </Container>
     </Navbar>
   )
 }
